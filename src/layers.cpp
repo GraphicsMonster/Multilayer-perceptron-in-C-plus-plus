21ec5cf8fc27afc551ef69b0f4767ed3d6a41494
Saksham Gupta <www.rameshgupta67@gmail.com>
2024-06-08 13:56:52 +0530

    using matrices everywhere now

--- src/layers.cpp
+++ src/layers.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
-#include "./header_files/math.hpp"
-#include "./header_files/activations.hpp"
+#include "../include/math.hpp"
+#include "../include/activations.hpp"
 
 using namespace std;
 
@@ -14,13 +14,13 @@ class FCLayer
         vector<vector<double>> weights;
         vector<double> biases;
         string activation;
-        vector<double> raw;
+        vector<vector<double>> raw;
         vector<vector<double>> weight_der;
         vector<double> bias_der;
     
     public:
 
-        vector<double> input_data;
+        vector<vector<double>> input_data;
 
         FCLayer(int input_dim, int num_neurons, const string& activation){
             this->weights = vector<vector<double>>(input_dim, vector<double>(num_neurons, 0));
@@ -29,37 +29,31 @@ class FCLayer
         }
 
 
-        vector<double> forward(vector<double> data)
+        vector<vector<double>> forward(vector<vector<double>> data)
         {
             this->input_data = data;
-            vector<double> output = Multiply_matrices(data, this->weights);
-            output = Add_vectors(output, this->biases);
+            vector<vector<double>> output = Multiply_matrices(data, this->weights);
+            output = Add_biases(output, this->biases);
             this->raw = output;
             output = applyActivation(output, this->activation);
             return output;
         }
 
-        vector<double> backprop(vector<double> dlda){
+        vector<vector<double>> backprop(vector<vector<double>> dlda){
 
-            vector<double> dldz = multiply_vectors(dlda, applyActivation_derivative(this->raw, this->activation));
+            vector<vector<double>> dldz = Multiply_matrices(dlda, applyActivation_derivative(this->raw, this->activation));
 
             this->weight_der = vector<vector<double>>(this->weights.size(), vector<double>(this->weights[0].size()));
 
-            for(int i = 0; i<weights.size(); i++){
-                for(int j = 0; j<weights[0].size(); j++){
-                    this->weight_der[i][j] = this->input_data[i] * dldz[j];
+            this->weight_der = Multiply_matrices(dldz, this->input_data);
+
+            for(int i = 0; i<dldz.size(); i++){
+                for(int j = 0; j<dldz[0].size(); j++){
+                    this->bias_der[j] += dldz[i][j];
                 }
             }
 
-            this->bias_der = dldz;
-
-            vector<double> dldx(this->input_data.size(), 0.0);
-            for (size_t i = 0; i < dldx.size(); ++i) {
-                for (size_t j = 0; j < dldz.size(); ++j) {
-                    dldx[i] += dldz[j] * weights[i][j];
-                }
-            }
-
+            vector<vector<double>> dldx = Multiply_matrices(dldz, this->weights);
             return dldx;
 
         }
@@ -77,3 +71,5 @@ class FCLayer
         }
 
 };
+
+// TODO: Edit everything such that vectors are represented as 2 dimensional matrices with shapes (1, something)
--- src/mlp.cpp
+++ src/mlp.cpp
@@ -13,47 +13,65 @@ class MLP
 
     public:
 
-        vector<double> forward(vector<double> x)
+        vector<vector<double>> forward(vector<vector<double>> x)
         {
             // Forward pass
-            vector<double> out = fc1.forward(x);
+            vector<vector<double>> out = fc1.forward(x);
             out = fc2.forward(out);
             out = fc3.forward(out);
             return out;
         }
 
-        double get_loss(vector<double> pred, vector<double> val)
+        double get_loss(vector<vector<double>> pred, vector<double> val)
         {
             // Going with MSE loss
+            vector<double> predictions(pred.size(), 0.0);
+            for(int i = 0; i<pred.size(); i++){
+                predictions[i] = pred[i][0];
+            }
+
             double loss = 0.0;
             for(int i = 0; i<pred.size(); i++){
-                loss += pow((pred[i] - val[i]), 2);
+                loss += pow((predictions[i] - val[i]), 2);
             }
 
             return loss/pred.size();
         }
 
         void backpass(vector<double> true_vals){
-            vector<double> dlda = vector<double>(true_vals.size(), 0.0);
+            vector<vector<double>> dlda(1, vector<double>(true_vals.size(), 0.0));
 
-            vector<double> inputs = this->fc1.input_data;
-            vector<double> preds = this->forward(inputs);
+            vector<vector<double>> preds = this->forward(this->fc1.input_data);
 
             double loss = this->get_loss(preds, true_vals);
 
             for(int i = 0; i<preds.size(); i++){
-                dlda[i] = (2 * (preds[i] - true_vals[i]))/preds.size();
+                dlda[0][i] = (2 * (preds[i][0] - true_vals[i]))/preds.size();
             }
 
             // Initiating backpass
-            vector<double>second_grad = this->fc3.backprop(dlda);
+            vector<vector<double>>second_grad = this->fc3.backprop(dlda);
             this->fc3.update_params(this->learning_rate);
 
-            vector<double>third_grad = this->fc2.backprop(second_grad);
-            this->fc3.update_params(learning_rate);
+            vector<vector<double>>third_grad = this->fc2.backprop(second_grad);
+            this->fc2.update_params(learning_rate);
 
             this->fc1.backprop(third_grad);
             this->fc1.update_params(learning_rate);
+        }
+
+        void train(int num_epochs, double learning_rate, vector<vector<double>> x, vector<double> y){
+            this->learning_rate = learning_rate;
+
+            for(int epoch = 0; epoch<num_epochs; epoch++){
+                vector<vector<double>> preds = this->forward(x);
+                this->backpass(y);
+                double loss = this->get_loss(preds, y);
+
+                if((epoch+1) % 100 == 0){
+                    cout << "Epoch: " << epoch << " || Loss: " << loss << "\n";
+                }
+            }
         }
 };
 
